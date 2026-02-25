9be3cecfc87698f9a9de9547aac0eaca5d8ce8cf
Cade Ryan McCaskill <cademccaskill@gmail.com>
2026-02-24 20:15:32 -0600

    creating post

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -8,6 +8,7 @@ import { CategoryForm } from "../components/catergories/newCategoryForm"
 import { TagList } from "../components/tag/TagList"
 import { TagForm } from "../components/tag/newTagForm"
 import { PostDetails } from "../components/posts/Posts"
+import { PostForm } from "../components/posts/PostForm"
  
 export const ApplicationViews = ({ token, setToken }) => {
   return <>
@@ -21,6 +22,7 @@ export const ApplicationViews = ({ token, setToken }) => {
         <Route path="/tags" element={<TagList />} />
         <Route path="/newtag" element={<TagForm />} />
         <Route path="/posts/:post_id" element={<PostDetails token={token} />} />
+        <Route path="/posts/new" element={<PostForm token={token} />} />
       </Route>
     </Routes>
   </>
