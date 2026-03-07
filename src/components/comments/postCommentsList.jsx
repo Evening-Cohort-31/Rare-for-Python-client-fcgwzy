054ae99e49c03b9dd44bb6f14e617c2e4fd8569c
Lea Edwards <leaxedw@gmail.com>
2026-03-07 10:01:06 -0600

    beginning the comments list

---
+++ src/components/comments/postCommentsList.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react"
+
+export const CommentList = () => {
+
+    return (
+        <div>
+            <div className="comment">
+                
+            </div>
+        </div>
+    )
+}
