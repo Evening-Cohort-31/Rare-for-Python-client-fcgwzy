b9d34686eef3ca18c082ad9aba3e888330f81c30
Cade Ryan McCaskill <cademccaskill@gmail.com>
2026-03-09 19:06:27 -0500

    Adding an edit form also well so they can change authers to admin

---
+++ src/components/User/UserEditForm.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { getUserById, updateUser } from "../../managers/userManager"
+import { SaveButton } from "../buttons/SaveButton.jsx"
+import { CancelButton } from "../buttons/CancelButton.jsx"
+
+export const UserEditForm = () => {
+    const [user, setUser] = useState(null)
+    const { userId } = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        getUserById(userId).then(setUser)
+    }, [userId])
+
+    const handleSave = () => {
+        updateUser(userId, user).then(() => {
+            navigate("/users")
+        })
+    }
+
+    if (!user) return <p>Loading...</p>
+
+    return (
+        <div>
+            <h2>Edit User Type for {user.first_name} {user.last_name}</h2>
+
+            <label>User Type</label>
+            <select
+                value={user.is_admin}
+                onChange={(event) => setUser({ ...user, is_admin: parseInt(event.target.value) })}
+            >
+                <option value={0}>Author</option>
+                <option value={1}>Admin</option>
+            </select>
+
+            <SaveButton onClick={handleSave} />
+            <CancelButton onClick={() => navigate("/users")} />
+        </div>
+    )
+}
