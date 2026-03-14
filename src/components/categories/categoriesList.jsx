b99044b38737c94bde60d18376504a624bc3a832
Lea Edwards <leaxedw@gmail.com>
2026-03-13 19:29:07 -0500

    changed file names to capitals to hopefully fix the imports

--- src/components/categories/categoriesList.jsx
+++ src/components/categories/categoriesList.jsx
@@ -5,8 +5,8 @@
 import { useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { getAllCategories, deleteCategory } from "../../managers/CategoryManager";
-import { EditButton } from "../buttons/EditButton.jsx"
-import { DeleteButton } from "../buttons/DeleteButton.jsx"
+import { EditButton } from "../buttons/EditButton";
+import { DeleteButton } from "../buttons/DeleteButton";
 
 const initialCategoryState = {
     id: 0,
